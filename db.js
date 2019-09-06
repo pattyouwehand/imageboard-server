6c78e1029ce21a8031599c9440c62e1d09701b4a
pattyouwehand <pattyouwehand@gmail.com>
2019-09-06 11:24:44 +0200

    setup sequelize

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+
+const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/postgres'
+const db = new Sequelize(databaseURL)
+
+db
+  .sync({ force: false })
+  .then(() => console.log('Database connected'))
+  .catch(console.error)
+
+module.exports = db
